07af29517d185cea6224234ff3a0df1e613007ac
Gabriel Seals <sealsgj@gmail.com>
2019-09-23 19:52:23 -0500

    merge time

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,13 +1,27 @@
+const bookInfo = {
+    price: 24.99,
+    title: "Somehow I Manage",
+    image: "./assets/images/book.jpg"
+  };
+
 const printToDom = (divId, toPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = toPrint;
 };
 
+const addToCartEvent = () => {
+    console.log('added to cart');
+}
+
 const makeStore = () => {
-    const domString = 'hi new store';
+        let domString = '<h2>Our only book:</h2>';
+        domString += '<p>Buy it now:</p>';
+        domString += `<h3>$${bookInfo.price}</h3>`;
+        domString += `<img src=${bookInfo.image} alt="book cover"/>`;
+        domString += '<button id="cart-button" class="btn btn-danger col-12">Add to cart</button>';
     printToDom('store-container',domString);
+    document.getElementById('cart-button').addEventListener('click', addToCartEvent)
 };
-makeStore();
 
 // export dafault { makeStore: makeStore };
 export default { makeStore };
